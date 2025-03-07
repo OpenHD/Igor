7bc080904b7ca3e6a82fd6952cc2790f3fef311d
LimitlessGreen <limitlessgreen@outlook.de>
2025-03-07 16:45:21 +0100

    Fix routing

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 import { ImageManagementComponent } from './image-management/image-management.component';
 
 export const routes: Routes = [
-  { path: 'front', component: ImageManagementComponent },
-  { path: 'front/images', component: ImageManagementComponent },
+  { path: '', component: ImageManagementComponent },
+  { path: 'images', component: ImageManagementComponent },
 ];
