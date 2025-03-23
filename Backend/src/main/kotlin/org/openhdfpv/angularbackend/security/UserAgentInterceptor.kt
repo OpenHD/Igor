e8e9118dce3b6d3ae34df4e9590b16459b27f3cd
LimitlessGreen <limitlessgreen@outlook.de>
2025-03-23 02:54:17 +0100

    Turn filter off for now

--- Backend/src/main/kotlin/org/openhdfpv/angularbackend/security/UserAgentInterceptor.kt
+++ Backend/src/main/kotlin/org/openhdfpv/angularbackend/security/UserAgentInterceptor.kt
@@ -2,18 +2,25 @@ package org.openhdfpv.angularbackend.security
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
 
-
 @Component
-class UserAgentInterceptor(private val userAgentService: UserAgentService) : HandlerInterceptor {
+class UserAgentInterceptor(
+    private val userAgentService: UserAgentService,
+    @Value("\${useragent.interceptor.enabled}") private val isEnabled: Boolean
+) : HandlerInterceptor {
 
     override fun preHandle(
         request: HttpServletRequest,
         response: HttpServletResponse,
         handler: Any
     ): Boolean {
+        if (!isEnabled) {
+            return true
+        }
+
         val userAgent = request.getHeader("User-Agent")
         return if (userAgentService.isAllowedUserAgent(userAgent)) {
             true
